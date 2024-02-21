74f47af9f7e336299183c85ac60de3810161442d
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2024-02-21 18:51:36 -0500

    Update ProfileManager.ts

--- src/RPG/ProfileManager.ts
+++ src/RPG/ProfileManager.ts
@@ -1,5 +1,24 @@
-export default class {
-    constructor() {
+import { QuickDB } from "quickdb";
+
+class Profile {
+    Money: Number;
+    Souls: Number;
+    HP: Number;
+    constructor(id: string) {
         
+    }
+}
+
+export default class {
+    db: QuickDB
+    constructor(client) {
+        this.db = client.db;
+    }
+    Started(id: string): boolean {
+        return false;
+    }
+    Get(id: string) {
+        if (!this.Started(id)) return null;
+        return new Profile(id);
     }
 }
