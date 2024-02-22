6c32da5898879ca44de5384ef118c534b052d3e8
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2024-02-21 20:45:37 -0500

    Update ProfileManager.ts

--- src/RPG/ProfileManager.ts
+++ src/RPG/ProfileManager.ts
@@ -7,12 +7,29 @@ class ProfileStats {
     Stamina: Number;
     Endurance: Number;
     PowerScale: Number;
-    constructor(Stats: any) {
+    Data: any;
+    Changes: Array<string>;
+    db: QuickDB;
+    id: string;
+    StData: any;
+    constructor(Data: any, db: QuickDB, id: string) {
+        const Stats = Data.Stats;
+        this.StData = Stats;
         this.MAXHP = Stats.MAXHP;
         this.HP = Stats.HP;
         this.Stamina = Stats.Stamina;
         this.Endurance = Stats.Endurance;
         this.PowerScale = Stats.PowerScale;
+        this.Data = Data;
+        this.db = db;
+        this.id = id;
+    }
+    async UpdateStats() {
+        for (const change of this.Changes) {
+            this.StData[change] = this[change as keyof this];
+        }
+        this.Data.Stats = this.StData;
+        await this.db.set(`RPG/${this.id}`, this.Data);
     }
 }
 
@@ -23,19 +40,43 @@ class Inventory {
     Potions: Array<string>;
     Items: ItemsPropretyTyping;
     Armors: ItemsPropretyTyping;
-    constructor(Data: any) {
-        for (const Weapon of Data.Weapons) {
+    Data: any;
+    db: QuickDB;
+    id: string;
+    constructor(Data: any, db: QuickDB, id: string) {
+        this.id = id;
+        this.db = db;
+        const Items = Data.Inventory;
+        for (const Weapon of Items.Weapons) {
             this.Weapons[Weapon.name] = Weapon.Durability;
         }
-        for (const Potion of Data.Potions) {
+        for (const Potion of Items.Potions) {
             this.Potions.push(Potion);
         }
-        for (const item of Data.Items) {
+        for (const item of Items.Items) {
             this.Items[item.name] = item.amount;
         }
-        for (const armor of Data.amors) {
+        for (const armor of Items.amors) {
             this.Armors[armor.name] = armor.Durability;
         }
+        this.Data = Data;
+    }
+    async UpdateInventory() {
+        [{name: "", Durability: 1}]
+        let DBweapons = [];
+        for (const Weapon of this.Weapons) {
+            DBweapons.push({name: Weapon, Durability: this.Weapons[Weapon]});
+        }
+        let DBarmors = [];
+        for (const Armor of this.Armors) {
+            DBarmors.push({name: Armor, Durability: this.Armors[Armor]});
+        }
+        let DBitems = [];
+        for (const Item of this.Items) {
+            DBitems.push({"name": Item, "amount": this.Items[Item]});
+        }
+        this.Data.Inventory = {"Weapons": DBweapons, "armors": DBarmors, "Potions": this.Potions, "Items": DBitems}
+        await this.db.set(`RPG/${this.id}`, this.Data);
     }
 }
 
@@ -50,7 +91,10 @@ class Profile {
     EquipedArmor: string;
     Stats: ProfileStats;
     Inventory: Inventory;
+    rawData: any;
+    Changes: Array<string>;
     constructor(Data: any, db: QuickDB, id: string) {
+        this.rawData = Data;
         this.id = id;
         this.db = db;
         this.Money = Data.Money;
@@ -58,8 +102,21 @@ class Profile {
         this.Lang = Data.lang;
         this.EquipedWeapon = Data.equipedWeapon;
         this.EquipedArmor = Data.equipedArmor;
-        this.Stats = new ProfileStats(Data.Stats);
-        this.Inventory = new Inventory(Data.Inventory);
+        this.Stats = new ProfileStats(Data, db, id);
+        this.Inventory = new Inventory(Data, db, id);
+    }
+    async UpdateUser() {
+        for (const change of this.Changes) {
+            this.rawData[change] = this[change as keyof this];
+        }
+        await this.db.set(`RPG/${this.id}`, this.rawData);
+    }
+    async EquipWeapon(name: string): Promise<boolean> {
+        if (!this.Inventory.Weapons[name]) return false;
+        this.EquipedWeapon = name;
+        this.Changes = ["EquipedWeapon"];
+        await this.UpdateUser();
+        return true;
     }
 }
 
